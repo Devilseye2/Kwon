00c8c585d156bb0d4fe58b6d9ed536762d4d835e
Devilseye2 <nickmerlin5@gmail.com>
2024-08-08 00:40:44 +0300

    first commit

---
+++ settings.js
@@ -0,0 +1,9 @@
+//Ubah disini bro
+//SUBSCRIBE YT : Delta Tech
+global.baileys1 = require('@whiskeysockets/baileys') 
+global.prefa = ['','!','.',',','⚡','🤡']
+global.nocreator = ['6287842909138']
+global.owner = ['6287842909138']
+global.botname = 'Yonima'
+global.sticker1 = "yonima"
+global.sticker2 = "ayonima"
